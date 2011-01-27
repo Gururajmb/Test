88a7fd21d54397508b8723e6bdc1a0b9a3cc63e9
Your Name <guru977@gmail.com>
2011-01-27 09:48:14 +0530

    Java-Prgram

---
+++ SumOfAllMultiple3N5Test.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+import static junit.framework.Assert.assertEquals;
+
+public class SumOfAllMultiple3N5Test {
+
+    @Test
+    public void testSumOfMultiplesOf3And5(){
+        assertEquals(233168, new NumberRange(1000).sumOfMultiplesOf(3, 5));
+    }
+}
